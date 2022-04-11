8864f85b325c9f1a9cf48316646a3edb4623007c
gary-beautypie <71257526+gary-beautypie@users.noreply.github.com>
2022-04-11 15:33:56 +0200

    Update format_handler.py

--- tap_spreadsheets_anywhere/format_handler.py
+++ tap_spreadsheets_anywhere/format_handler.py
@@ -17,6 +17,11 @@ class InvalidFormatError(Exception):
 
 
 def get_streamreader(uri, universal_newlines=True,newline='',open_mode='r'):
+    if uri.startswith('http'):
+        import urllib.request
+        new_uri = "tempfile" + uri.split('.')[-1]
+        urllib.request.urlretrieve(uri, new_uri)
+        uri = new_uri
     streamreader = smart_open.open(uri, open_mode, newline=newline, errors='surrogateescape')
     if not universal_newlines and isinstance(streamreader, StreamReader):
         return monkey_patch_streamreader(streamreader)
